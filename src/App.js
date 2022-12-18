4dab82f445349fe06607ece927270aa2f1bffa01
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-12-18 18:58:18 +0530

    Done signup part.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from "./firebase";
+import { db, auth } from "./firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { collection, getDocs } from "firebase/firestore";
+import { Button, Modal, Box, Input } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
 
   async function getData() {
     const querySnapshot = await getDocs(collection(db, "posts"));
-    setPosts(querySnapshot.docs.map((doc) => doc.data()));
+    setPosts(
+      querySnapshot.docs.map((doc) => ({ id: doc.id, post: doc.data() }))
+    );
   }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        //user has logged out...
+        setUser(null);
+      }
+    });
+
+    return () => {
+      //cleanup
+      unsubscribe();
+    };
+  }, [user, username]);
 
   useEffect(() => {
     getData();
   }, []);
 
+  const signup = async (e) => {
+    e.preventDefault();
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((authUser) => {
+        return updateProfile(authUser, {
+          displayName: username,
+        })
+          .then(() => {
+            console.log("Profile Updated!");
+          })
+          .catch((err) => {
+            console.log("Error!");
+          });
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="Username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="Email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signup}>
+              Sign Up
+            </Button>
+          </form>
+        </Box>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -26,11 +123,13 @@ function App() {
         />
       </div>
 
-      <h1>Hello</h1>
+      <Button onClick={() => setOpen(true)}>Sign up</Button>
 
-      {posts.map((post) => (
+      <h1>Hello Eliminator! Let's build an instagram clone.</h1>
+
+      {posts.map(({ id, post }) => (
         <Post
-          key={post.id}
+          key={id}
           username={post.username}
           caption={post.caption}
           imageUrl={post.imageUrl}
